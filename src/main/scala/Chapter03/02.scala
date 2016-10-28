94b090995dbee585fe94edc08f301728cae30dbe
jianye <jianye168@gmail.com>
2016-10-28 10:59:46 -0700

    third question solution update

--- src/main/scala/Chapter03/02.scala
+++ src/main/scala/Chapter03/02.scala
@@ -5,17 +5,20 @@ def swap(a:Array[Int]): Array[Int] = {
     a
 }
 
+def apply(a:Array[Int], i:Int) = {
+    var tmp = a(i)
+    a(i) = a(i + 1)
+    a(i + 1) = tmp
+}
+
 def swapAnother(a:Array[Int]): Array[Int] = {
+    0, 1, 2, 3, 4, 5 => a.size = 6 => a.size - 1 = 5 => [0,4] => 0, 2, 4
+    0, 1, 2, 3, 4, 5, 6 => a.size = 7 => a.size - 1 = 6 => [0, 5] =>0, 2, 4
     for (i <- 0 until (a.size - 1, 2)) {
         var tmp = a(i)
         a(i) = a(i + 1)
         a(i + 1) = tmp
     }
+    //Assignment returns Unit, need to explicitly return the array.
     a
-}
-
-def apply(a:Array[Int], i:Int) = {
-    var tmp = a(i)
-    a(i) = a(i + 1)
-    a(i + 1) = tmp
 }
--- src/main/scala/Chapter03/03.scala
+++ src/main/scala/Chapter03/03.scala
@@ -1,5 +1,23 @@
 //Write a function to swap the adjacent two elements of an integer array
 //Generate a new array
-def swap(a:Array[Int]): Array[Int] = {
-    for (a <- 0 until (a.size - 1, 2)) yield a(i + 1), a(i)
+//yield returns scala.collection.immutable.IndexedSeq[Int]
+//yield has to follow for(....), for(...) ... match yield is illegal.
+def swap(a:Array[Int]) = {
+    for (i <- 0 until a.size) yield
+            if (i % 2 == 0)
+                if (i < a.size - 1) a(i + 1) else a(i)
+            else a(i- 1)
+}
+//Pattern match returns a value under different cases
+// i match {
+    //case ... => return value1
+    //case <value> pattern match i with the value, cannot use case<statement/expression>
+    //case ... =>  return value 2
+//}
+def swap(a:Array[Int]) = {
+    for (i <- 0 until a.size) yield i % 2 match {
+        case 0 if i < a.size - 1 => a(i + 1)
+        case 0 if i == a.size - 1 => a(i)
+        case _ => a(i - 1)
+    }
 }
